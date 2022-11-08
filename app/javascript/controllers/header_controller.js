c816e585cf1265a3853ff89e29db7844d98ea5b2
cm <cecillemanalang@gmail.com>
2022-11-09 00:26:41 +0800

    part 2 nav bar

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from "el-transition"
+
+export default class extends Controller {
+  static targets = ["openUserMenu"];
+
+  connect() {
+    this.openUserMenuTarget.addEventListener(
+      "click",
+      this.toggleDropDownMenu
+    );
+  }
+
+  toggleDropDownMenu() {
+    toggle(document.getElementById('menu-dropdown-items'));
+  }
+}
