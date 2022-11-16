90a3e112faaae7d46fb9f7d57e97398c4f608d11
cm <cecillemanalang@gmail.com>
2022-11-17 00:15:52 +0800

    part 3 modal

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,14 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
-import { enter, leave, toggle } from "el-transition"
+import { toggle } from "el-transition"
 
 export default class extends Controller {
-  static targets = ["openUserMenu"];
+  static targets = ["openUserMenu", "userAuthLink"];
 
   connect() {
-    this.openUserMenuTarget.addEventListener(
-      "click",
-      this.toggleDropDownMenu
-    );
+    this.openUserMenuTarget.addEventListener("click", this.toggleDropDownMenu);
+    this.userAuthLinkTargets.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.getElementById('modal-trigger').click();
+      });
+    });
   }
 
   toggleDropDownMenu() {
