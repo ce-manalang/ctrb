2336acbde87efa35663e0db6535952dd163c2437
cm <cecillemanalang@gmail.com>
2022-11-26 12:33:58 +0800

    wip

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'airbnb-pink'
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
